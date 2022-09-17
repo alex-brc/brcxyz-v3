cf7887b086802804a8c521bd940d9512d4249a99
Alexandru Bracau <abracau@deloittece.com>
2022-09-17 22:55:36 +0300

    Core components, trying things out

---
+++ src/lib/components/cloud.svelte
@@ -0,0 +1,19 @@
+<script>
+    import cloud from "$lib/assets/cloud.svg";
+
+    export let top = 0;
+    export let left = 0;
+    $: cssVarStyles = `left:${left}; top:${top};`;
+</script>
+
+<img class="cloud" src={cloud} alt="cloud" style={cssVarStyles} />
+
+<style>
+    .cloud {
+        position: absolute;
+        width: 20vw;
+        height: auto;
+        min-width: 100px;
+        max-width: 300px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Cloud from "$lib/components/cloud.svelte";
+    let mode = "light";
+    let switchModes = () => {
+        if (mode == "light") mode = "dark";
+        else if (mode == "dark") mode = "light";
+        else throw Error("Unknown target state: " + mode);
+    };
+</script>
+
+<Cloud top="" left="-5%" />
+
+<main>
+    <h1>{mode}</h1>
+    <button on:click={switchModes}>Switch Modes</button>
+</main>
+
+<style>
+    main {
+        height: 100vh;
+        margin: 0;
+        text-align: center;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4rem;
+        font-weight: 100;
+        line-height: 1.1;
+    }
+</style>
